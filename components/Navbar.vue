a7e120be981baa2b45b9ca71113090cfdf9e749c
o.sharafan.stc <o.sharafan.stc@innopolis.university>
2022-05-10 22:14:49 +0300

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Nuxt SSR</a>
+
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+    export default {
+        name: "NavbarMy",
+    }
+</script>
+
+<!--<style scoped>-->
+
+<!--</style>-->
---
+++ components/Tutorial.vue
@@ -0,0 +1,16 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+}
+</script>
+
+<!--<style>-->
+
+<!--</style>-->
